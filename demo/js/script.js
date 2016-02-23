ab65d19d871e7c3c9901ae88a368fe50628a34cd
caddshac <caddshac@yahoo.com>
2016-02-23 10:01:25 -0600

    Added files via upload

---
+++ demo/js/script.js
@@ -0,0 +1,44 @@
+var map = L.map('map').setView([30, -90], 4);
+
+var streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
+		}).addTo(map);
+
+var precipitation = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
+		    layers: 'nexrad-n0r-900913',
+		    format: 'image/png',
+		    transparent: true,
+		    attribution: "Weather data © 2012 IEM Nexrad"
+		}).addTo(map);
+
+
+var tornadowarnings = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_shortduration_hazards_warnings_time/MapServer/WMSServer", {
+				layers: '3',
+				format: 'image/png',
+				transparent: true,
+				attribution: "NOAA Warnings, Watches and Advisories"
+		}).addTo(map);
+
+
+var relativehumidity = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_relhumidity_offsets/MapServer/WMSServer", {
+				layers: '1',
+				format: 'image/png',
+				transparent: true,
+				attribution: "NOAA Relative Humidity",
+				opacity: 0.25
+		}).addTo(map);
+
+
+// Create an object with Layers for each basemap
+var baseLayers = {
+				    "Streets": streets
+				};
+
+var overlays = {
+				    "Precipication": precipitation,
+				    "Tornado Warnings": tornadowarnings,
+						"Relative Humidity": relativehumidity
+				};
+
+L.control.layers(baseLayers, overlays).addTo(map);
