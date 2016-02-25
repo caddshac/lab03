bedb00d1b6547a3f7789792fd6abb5ae0fad7a04
caddshac <caddshac@yahoo.com>
2016-02-25 10:08:49 -0600

    Update script.js

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([35, -90], 4);
+var map = L.map('map').setView([35, -95], 4);
 
 var streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
